21a8253df5978e8b7d3b9c969c5be86f0f62d80b
Celsoft <maksim_blg@mail.ru>
2020-11-02 11:05:52 +0300

    new

--- index.php
+++ index.php
@@ -58,7 +58,7 @@ function curlProxy($mirror)
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
     curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
-    curl_setopt($ch, CURLOPT_USERAGENT, $userAgent . ' (GOOGLE_APP_ENGINE)');
+    curl_setopt($ch, CURLOPT_USERAGENT, $userAgent . ' AppEngine-Google');
     $result = curl_exec($ch);
     $result = str_replace($oldDomain, $redirectDomain, $result);
     $info = curl_getinfo($ch);
